05d7e213d275e3d240916cc490261e291c1207e8
Elijah-24 <145415614+Elijah-24@users.noreply.github.com>
2024-07-25 22:10:27 -0500

    Update server.js

--- elijahs server/server.js
+++ elijahs server/server.js
@@ -12,12 +12,12 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "cosc4353-project" // database 
+    database: "volunteer_management" // database 
 }) 
 
 // Event routes
 app.get("/", (req, res) => {
-    const sql = "SELECT * FROM event"; // `event` is one of my tables that keeps the events and all of their information
+    const sql = "SELECT * FROM eventdetails"; // `event` is one of my tables that keeps the events and all of their information
     db.query(sql, (err, data) => {
         if(err) return res.json("Error");
         return res.json(data);
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 })
 
 app.post('/create', (req, res) => { // Creates an event in the `event` table
-    const sql = "INSERT INTO event (`Name`, `Description`, `Location`, `RequiredSkills`, `Urgency`, `Date`) VALUES (?)";
+    const sql = "INSERT INTO eventdetails (`event_name`, `description`, `location`, `required_skills`, `urgency`, `eventDate`) VALUES (?)";
     const values = [
         req.body.name,
         req.body.description,
@@ -40,8 +40,8 @@ app.post('/create', (req, res) => { // Creates an event in the `event` table
     })
 })
 
-app.put('/update/:id', (req, res) => { // Updates an event when a change needs to be made
-    const sql = "update event set `Name` = ?, `Description` = ?, `Location` = ?, `RequiredSkills` = ?, `Urgency` = ?, `Date` = ? where ID = ?";
+app.put('/update/:event_id', (req, res) => { // Updates an event when a change needs to be made
+    const sql = "update eventdetails set `event_name` = ?, `description` = ?, `location` = ?, `required_skills` = ?, `urgency` = ?, `eventDate` = ? where event_id = ?";
     const values = [
         req.body.name,
         req.body.description,
@@ -51,19 +51,19 @@ app.put('/update/:id', (req, res) => { // Updates an event when a change needs t
         req.body.date
     ]
 
-    const id = req.params.id;
+    const event_id = req.params.event_id;
 
-    db.query(sql, [...values, id], (err, data) => {
+    db.query(sql, [...values, event_id], (err, data) => {
         if(err) return res.json("Error");
         return res.json(data);
     })
 })
 
-app.delete('/event/:id', (req, res) => { // Deletes the event from the database
-    const sql = "DELETE FROM event WHERE ID = ?";
-    const id = req.params.id;
+app.delete('/event/:event_id', (req, res) => { // Deletes the event from the database
+    const sql = "DELETE FROM eventdetails WHERE event_id = ?";
+    const event_id = req.params.event_id;
 
-    db.query(sql, [id], (err, data) => {
+    db.query(sql, [event_id], (err, data) => {
         if(err) return res.json("Error");
         return res.json(data);
     })
@@ -71,22 +71,22 @@ app.delete('/event/:id', (req, res) => { // Deletes the event from the database
 
 // Volunteer routes
 app.get("/volunteer", (req, res) => {
-    const sql = "SELECT * FROM volunteer"; // `volunteer` is another table in my database that has everything about a volunteer as well as a column to track what event they're matched with
+    const sql = "SELECT * FROM userprofile"; // `volunteer` is another table in my database that has everything about a volunteer as well as a column to track what event they're matched with
     db.query(sql, (err, data) => {
         if(err) return res.json("Error");
         return res.json(data);
     })
 })
 
-app.put('/match/:id', (req, res) => { // Matches volunteer with an event by updating their `Event` column with the name of the event
-    const sql = "update volunteer set `Event` = ? where ID = ?";
+app.put('/match/:user_id', (req, res) => { // Matches volunteer with an event by updating their `Event` column with the name of the event
+    const sql = "update userprofile set `event_match` = ? where user_id = ?";
     const values = [
         req.body.matchedEvent
     ]
 
-    const id = req.params.id;
+    const user_id = req.params.user_id;
 
-    db.query(sql, [...values, id], (err, data) => {
+    db.query(sql, [...values, user_id], (err, data) => {
         if(err) return res.json("Error");
         return res.json(data);
     })
