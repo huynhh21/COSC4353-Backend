0288b03bdc0d7485caacd3a2ab83f1f6ea382ceb
nicholaskim7 <154484441+nicholaskim7@users.noreply.github.com>
2024-08-01 08:06:17 -0500

    Update server.js  error in create user fixed

--- Backend/server.js
+++ Backend/server.js
@@ -129,7 +129,7 @@ app.post('/create', (req, res) => {
                             return res.json({ message: "Commit Error" });
                         }
 
-                        res.status(201).json({ message: "User created successfully", userId });
+                        res.status(201).json({ message: "User created successfully...", userId });
                     });
                 });
             });
